27101833c38bd00d4c0ebf22766f9473511e6c0d
Trey Stafford <trey.stafford@colorado.edu>
2024-03-27 17:27:41 -0600

    WIP cleanup some commented out code, try to use pandas

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -7,35 +7,26 @@ pre-defined transformations focus on e.g., reading a variable from many nc files
 and then aggregating them into zarr stores.
 """
 import apache_beam as beam
-from pangeo_forge_recipes.patterns import FilePattern, FileType
+from pangeo_forge_recipes.patterns import FilePattern, FileType, Index
 from pangeo_forge_recipes.transforms import OpenURLWithFSSpec
 
 
 INPUT_URL = "https://arcticdata.io/metacat/d1/mn/v2/object/urn%3Auuid%3A31162eb9-7e3b-4b88-948f-f4c99f13a83f"
 OUTPUT_PATH = "foo.txt"
 
-# from .open_pandas import OpenCsvWithPandas
+class WriteCSV(beam.DoFn):
+    def process(self, pcoll):
+        import pandas as pd
+        breakpoint()
+        local_fs = pcoll[1]
+        # data = pd.read_csv(local_fs.f)
+        # data.write_csv(OUTPUT_PATH)
+        ...
 
-# Type as pcoll containing empty dict and fsspec.localfileopener
-def write_csv(pcoll):
-    pcoll[1].write(OUTPUT_PATH)
-    return OUTPUT_PATH
-
-
-# @beam.ptransform_fn
-# def WriteCsv(pcoll: beam.PCollection):
-#     return beam.ParDo(lambda: 
-#     ...
-pattern = FilePattern(
-    lambda: INPUT_URL,
-    file_type=FileType.unknown,
-)
+pattern = FilePattern(lambda: INPUT_URL, file_type=FileType.unknown)
 
 recipe = (
     beam.Create(pattern.items())
     | OpenURLWithFSSpec()
-    # | OpenCsvWithPandas()
-    # | SomeCustomProcessing()
-    # | beam.io.WriteToText("foo.txt")
-    | beam.ParDo(write_csv)
+    | beam.ParDo(WriteCSV())
 )
