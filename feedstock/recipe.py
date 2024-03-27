685dca2022472f7b8766d973305defa36d57d214
Matt Fisher <mfisher87@gmail.com>
2024-03-27 15:42:54 -0600

    WIP attempt to write out CSV data

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -7,21 +7,35 @@ pre-defined transformations focus on e.g., reading a variable from many nc files
 and then aggregating them into zarr stores.
 """
 import apache_beam as beam
-from pangeo_forge_recipes.patterns import FilePattern
+from pangeo_forge_recipes.patterns import FilePattern, FileType
+from pangeo_forge_recipes.transforms import OpenURLWithFSSpec
 
 
-# In pangeo-forge, this function would normally take arguments corresponding to
-# the variables specified by a `MergeDim` instance. We're just fetching a single
-# csv file.
-def make_full_path():
-    return "https://arcticdata.io/metacat/d1/mn/v2/object/urn%3Auuid%3A31162eb9-7e3b-4b88-948f-f4c99f13a83f"
+INPUT_URL = "https://arcticdata.io/metacat/d1/mn/v2/object/urn%3Auuid%3A31162eb9-7e3b-4b88-948f-f4c99f13a83f"
+OUTPUT_PATH = "foo.txt"
+
+# from .open_pandas import OpenCsvWithPandas
+
+# Type as pcoll containing empty dict and fsspec.localfileopener
+def write_csv(pcoll):
+    pcoll[1].write(OUTPUT_PATH)
+    return OUTPUT_PATH
 
 
-pattern = FilePattern(make_full_path)
-for index, fname in pattern.items():
-    print(index, fname)
+# @beam.ptransform_fn
+# def WriteCsv(pcoll: beam.PCollection):
+#     return beam.ParDo(lambda: 
+#     ...
+pattern = FilePattern(
+    lambda: INPUT_URL,
+    file_type=FileType.unknown,
+)
 
 recipe = (
     beam.Create(pattern.items())
-    | beam.io.WriteToText("foo.txt")
+    | OpenURLWithFSSpec()
+    # | OpenCsvWithPandas()
+    # | SomeCustomProcessing()
+    # | beam.io.WriteToText("foo.txt")
+    | beam.ParDo(write_csv)
 )
