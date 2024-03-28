a73f9af11f44c5adbebfd81424a5232a69c2ca99
Trey Stafford <trey.stafford@colorado.edu>
2024-03-28 16:16:50 -0600

    Write out gpkg w/ geopandas

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -15,16 +15,20 @@ from pangeo_forge_recipes.transforms import OpenURLWithFSSpec
 
 INPUT_URL = "https://arcticdata.io/metacat/d1/mn/v2/object/urn%3Auuid%3A31162eb9-7e3b-4b88-948f-f4c99f13a83f"
 # We ran into a blocker here:
-# This writes out to "./foo.txt" in the current directory because only
+# This writes out to "./foo.gpkg" in the current directory because only
 # zarr-specific writers support the injection of runner config like
 # `c.TargetStorage.root_path`. See `pangeo-forge-runner.plugin`.
-OUTPUT_PATH = "foo.txt"
+OUTPUT_PATH = "foo.gpkg"
 
 def write_csv(pcoll):
     import pandas as pd
+    import geopandas
+
     local_fs = pcoll[1]  # <LocalFileOpener>
     df = pd.read_csv(local_fs.f)
-    df.to_csv(OUTPUT_PATH)
+    geom = geopandas.points_from_xy(df.Longitude, df.Latitude)
+    geo_df = geopandas.GeoDataFrame(df, geometry=geom)
+    geo_df.to_file(OUTPUT_PATH, crs="EPSG:4326")
 
     return OUTPUT_PATH
 
