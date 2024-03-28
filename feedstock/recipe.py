f425bc1c17ed659bb00b8c9f3c88aa72e9f002d8
Trey Stafford <trey.stafford@colorado.edu>
2024-03-28 16:00:50 -0600

    Write recipe closer to pangeo-forge style
    We were trying to figure out how the pangeo-forge-runner injects runner config
    into classes like `StoreToZarr`. We thought there was an injection occuring that
    relied on the pattern of defining a dataclass that inherits from PTransform.
    
    We ended up finding that the `pangeo-forge-runner` code is very specifically
    written to inject this config into the Zarr writers, and nothing else.

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -6,22 +6,33 @@ Still need to figure out how to write the transform/write steps. Pangeo-forges'
 pre-defined transformations focus on e.g., reading a variable from many nc files
 and then aggregating them into zarr stores.
 """
+from dataclasses import dataclass, field
+
 import apache_beam as beam
-from pangeo_forge_recipes.patterns import FilePattern, FileType, Index
+from pangeo_forge_recipes.patterns import FilePattern, FileType
 from pangeo_forge_recipes.transforms import OpenURLWithFSSpec
 
 
 INPUT_URL = "https://arcticdata.io/metacat/d1/mn/v2/object/urn%3Auuid%3A31162eb9-7e3b-4b88-948f-f4c99f13a83f"
+# We ran into a blocker here:
+# This writes out to "./foo.txt" in the current directory because only
+# zarr-specific writers support the injection of runner config like
+# `c.TargetStorage.root_path`. See `pangeo-forge-runner.plugin`.
 OUTPUT_PATH = "foo.txt"
 
+def write_csv(pcoll):
+    import pandas as pd
+    local_fs = pcoll[1]  # <LocalFileOpener>
+    df = pd.read_csv(local_fs.f)
+    df.to_csv(OUTPUT_PATH)
 
-class WriteCSV(beam.DoFn):
-    def process(self, pcoll):
-        import pandas as pd
-        local_fs = pcoll[1]  # <LocalFileOpener>
-        df = pd.read_csv(local_fs.f)
-        df.write_csv(OUTPUT_PATH)
-        # 'DataFrame' object has no attribute 'write_csv'
+    return OUTPUT_PATH
+
+
+@dataclass
+class WriteCSV(beam.PTransform):
+    def expand(self, pcoll):
+        return pcoll | beam.Map(write_csv)
 
 
 pattern = FilePattern(lambda: INPUT_URL, file_type=FileType.unknown)
@@ -29,5 +40,5 @@ pattern = FilePattern(lambda: INPUT_URL, file_type=FileType.unknown)
 recipe = (
     beam.Create(pattern.items())
     | OpenURLWithFSSpec()
-    | beam.ParDo(WriteCSV())
+    | WriteCSV()
 )
