2e186251dbe3663197a21ceaffbd24393ce05025
Matt Fisher <mfisher87@gmail.com>
2024-03-27 18:03:10 -0600

    Anotate current code behavior with comments

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -14,14 +14,15 @@ from pangeo_forge_recipes.transforms import OpenURLWithFSSpec
 INPUT_URL = "https://arcticdata.io/metacat/d1/mn/v2/object/urn%3Auuid%3A31162eb9-7e3b-4b88-948f-f4c99f13a83f"
 OUTPUT_PATH = "foo.txt"
 
+
 class WriteCSV(beam.DoFn):
     def process(self, pcoll):
         import pandas as pd
-        breakpoint()
-        local_fs = pcoll[1]
-        # data = pd.read_csv(local_fs.f)
-        # data.write_csv(OUTPUT_PATH)
-        ...
+        local_fs = pcoll[1]  # <LocalFileOpener>
+        df = pd.read_csv(local_fs.f)
+        df.write_csv(OUTPUT_PATH)
+        # 'DataFrame' object has no attribute 'write_csv'
+
 
 pattern = FilePattern(lambda: INPUT_URL, file_type=FileType.unknown)
 
